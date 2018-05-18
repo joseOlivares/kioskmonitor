4b4d2410d12f97f945c4a062c7334ffd78d2185a
joseOlivares <joseluis_503@yahoo.com>
2018-05-17 22:55:32 -0600

    Cambios en ruta bd

--- public/js/setServerIp.js
+++ public/js/setServerIp.js
@@ -5,12 +5,12 @@
 */
 
 var servidor={
-	IP:'http://192.168.159.134:3000' //IP y puerto del servidor de monitoreo, ex  'http://192.168.79.128:3000'
+	IP:'https://hardware-mon.herokuapp.com/' //IP y puerto del servidor de monitoreo, ex  'http://192.168.79.128:3000'
 };
 
 module.exports = {
-	dbServer: '10.3.5.129', 
-	user:'admin',
-	pass:'admin85',
-	db:'dbmonitoreo'
+	dbServer: 'sql307.epizy.com', 
+	user:'epiz_22099025@192.168.0.6',
+	pass:'test2018',
+	db:'epiz_22099025_monitor'
 }; //ip del servidor de base de datos
