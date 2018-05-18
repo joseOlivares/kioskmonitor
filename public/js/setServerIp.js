92cf41d0d3e3ab7ec5d4978c66ca10a76183337d
joseOlivares <joseluis_503@yahoo.com>
2018-05-17 22:23:05 -0600

    first commit

---
+++ public/js/setServerIp.js
@@ -0,0 +1,16 @@
+/*
+	LIbreria para definir la IP Estatica y puerto del servidor de monitoreo
+	Codificado  e implementado por Jose Luis Olivares, email: joseluiss_503@hotmail.com
+	Version 1.0,    Junio 2017
+*/
+
+var servidor={
+	IP:'http://192.168.159.134:3000' //IP y puerto del servidor de monitoreo, ex  'http://192.168.79.128:3000'
+};
+
+module.exports = {
+	dbServer: '10.3.5.129', 
+	user:'admin',
+	pass:'admin85',
+	db:'dbmonitoreo'
+}; //ip del servidor de base de datos
